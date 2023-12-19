b5859a32c58c943e88ed0072ce7ad43625be8a00
Monica Hany <126457536+Monica-hany@users.noreply.github.com>
2023-12-19 18:49:40 +0200

    inferring latches

--- Design_test.v
+++ Design_test.v
@@ -1,20 +1,39 @@
 module combined_violations();
+    wire enable, Data;
+    reg out;
     reg [1:0] state;
 
   // Unreachable Blocks
-    always @ (state) begin
-        if (state == 2'b00) begin
+    always @ (state) 
+    begin
+        if (state == 2'b00) 
+        begin
             data <= 1'b0;
-        end else if (state == 2'b10) begin
+        end 
+        else if (state == 2'b10) 
+        begin
             data <= 1'b1;
-        end else if (state == 2'b11) begin
+        end 
+        else if (state == 2'b11) 
+        begin
+
         end
     end
 
         // Un-initialized Register
-    initial begin
+    initial 
+    begin
         $display("%b", data); 
     end
+    
 
+    //inferring latches
+    always @ (enable) 
+    begin
+        if (enable) 
+        begin
+            out <= Data;
+        end
+    end
 
 endmodule
