b01d261e70a26720f82427b9ec9d03d47127233b
AHKSASE2002 <21p0153@eng.asu.edu.eg>
2023-12-19 18:30:17 +0200

    update: added the unreachable Blocks

--- Design_test.v
+++ Design_test.v
@@ -1,8 +1,5 @@
 module combined_violations();
-    reg [1:0] state;
-
-
-    // Unreachable Blocks
+  // Unreachable Blocks
     always @ (state) begin
         if (state == 2'b00) begin
             data <= 1'b0;
@@ -11,12 +8,5 @@ module combined_violations();
         end else if (state == 2'b11) begin
         end
     end
-
-
-    // Un-initialized Register
-    initial begin
-        $display("%b", data); 
-    end
-
 
 endmodule
